6a9f98fa4fe4ab0c07443ffdc5ef94181ab4beff
Siddhi <131698330+siddibavalekar@users.noreply.github.com>
2024-01-13 01:44:59 +0530

    Create an int type of Array
    Created an Array of int type and store 10 elements in array.

---
+++ Ex_arrayint.java
@@ -0,0 +1,24 @@
+//create an array of int type and store 10 elements in array
+
+class Ex_arrayint
+{
+public static void main(String args[])
+{
+
+int arr []=new int[10];
+arr[0]=5;
+arr[1]=10;
+arr[2]=15;
+arr[3]=20;
+arr[4]=25;
+arr[5]=30;
+arr[6]=35;
+arr[7]=40;
+arr[8]=45;
+arr[9]=50;
+
+System.out.println(arr[4]);
+
+
+}
+}
