05ebad9e186d856ca465952e5be67cd47e8a255b
Siddhi <131698330+siddibavalekar@users.noreply.github.com>
2024-01-13 01:47:18 +0530

    Create String Array
    Created an array of String type and store 10 elements in array .

---
+++ Ex_array_string.java
@@ -0,0 +1,24 @@
+//create an array of String type and store 10 elements in array
+
+class Ex_array_string
+{
+public static void main(String args[])
+{
+
+String arr []=new String[10];
+arr[0]="Swift";
+arr[1]="Nano";
+arr[2]="Creta";
+arr[3]="nexon";
+arr[4]="wagonr";
+arr[5]="brezza";
+arr[6]="Alto";
+arr[7]="Baleno";
+arr[8]="Dzire";
+arr[9]="Nano";
+
+System.out.println(arr[2]);
+
+
+}
+}
