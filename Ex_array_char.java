9b8248d276bf0da3fe83fae2be9554f282404fb8
Siddhi <131698330+siddibavalekar@users.noreply.github.com>
2024-01-13 01:49:26 +0530

    Create char type of Array
    Created an Array of Char Type and store 10 elements in array.

---
+++ Ex_array_char.java
@@ -0,0 +1,24 @@
+//create an array of char type and store 10 elements in array
+
+class Ex_array_char
+{
+public static void main(String args[])
+{
+
+char arr []=new char[10];
+arr[0]='z';
+arr[1]='a';
+arr[2]='b';
+arr[3]='c';
+arr[4]='d';
+arr[5]='e';
+arr[6]='f';
+arr[7]='g';
+arr[8]='h';
+arr[9]='i';
+
+System.out.println(arr[4]);
+
+
+}
+}
